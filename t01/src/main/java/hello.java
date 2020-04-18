9abaaa176223f9ebe811a41424aa4fdfda6990ee
ThreeNut <1769040047@qq.com>
2020-04-18 15:48:12 +0800

    2

---
+++ t01/src/main/java/hello.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+/**
+ * @author BinPeng
+ * @date 2020/4/18 15:47
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
